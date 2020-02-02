2b5fc839092dbcfc0446eba8390aaa16a96a1fd2
Víctor Hernández <vhernandezcastro@gmail.com>
2020-02-02 05:25:59 -0400

    fixed index.html serving in index.js

--- server/index.js
+++ server/index.js
@@ -6,8 +6,10 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === 'production') {
-    app.get('/', path.join(__dirname, '../client/build/index.html'));
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
 }
 
 app.use("/api/restaurants", require("./routes/api/restaurants"));
